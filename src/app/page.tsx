9b9befe3a1db2ddf5593fc542cf8a2638a765b7c
Tara Skoglund <skoglundtara@gmail.com>
2023-10-13 13:05:51 +0200

    home layout done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>hello world</h1>
+    <main className="flex min-h-screen flex-col items-center p-24 font-serif text-center">
+      <div>
+        <h1 className="text-4xl md:text-6xl px-20 py-6 border-b-2 border-slate-500">
+          Quiz
+        </h1>
+      </div>
+      <div className="mt-24">
+        <div>
+          <p className="my-16 text-base md:text-2xl">
+            Do our quiz and learn more.
+          </p>
+        </div>
+        <div>
+          <button className=" px-3 py-2 border-2 border-black rounded text-xs md:text-base whitespace-nowrap">
+            Let’s begin!
+          </button>
+        </div>
+      </div>
     </main>
   );
 }

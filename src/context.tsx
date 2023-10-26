4b3da78a421818cd54ac4547882a9391321c8a0f
Tara Skoglund <skoglundtara@gmail.com>
2023-10-26 09:50:40 +0200

    change context value and add provaider to layout

--- src/context.tsx
+++ src/context.tsx
@@ -1,4 +1,4 @@
-import { onAuthStateChanged, User } from "firebase/auth";
+import { IdTokenResult, onAuthStateChanged, User } from "firebase/auth";
 import {
   createContext,
   ReactNode,
@@ -9,10 +9,10 @@ import {
 import { auth } from "./firebase/config";
 
 interface AuthContextType {
-  user: User | null;
+  token: IdTokenResult | null;
 }
 
-const AuthContext = createContext<AuthContextType>({ user: null });
+const AuthContext = createContext<AuthContextType>({ token: null });
 
 export const useAuth = () => {
   return useContext(AuthContext);
@@ -23,18 +23,24 @@ interface AuthProviderProps {
 }
 
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
-  const [user, setUser] = useState<User | null>(null);
+  const [token, setToken] = useState<IdTokenResult | null>(null);
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      setUser(user);
+    const unsubscribe = onAuthStateChanged(auth, (user: User | null) => {
+      if (user) {
+        user.getIdTokenResult().then((idTokenResult) => {
+          setToken(idTokenResult);
+        });
+      } else {
+        setToken(null);
+      }
     });
 
     return () => unsubscribe();
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ token }}>{children}</AuthContext.Provider>
   );
 };
 
